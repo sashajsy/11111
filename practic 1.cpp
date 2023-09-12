853c36c72146b30c0a39f9498e80d5fa685a834a
sashajsy <115659897+sashajsy@users.noreply.github.com>
2023-09-13 00:01:14 +0300

    Add files via upload

---
+++ practic 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int a = 4;
+    double b = 300.1;
+    double result;
+
+    result = 7.2 * pow((a + b), (1 + pow(cos(a), 2)) * (1 - cos(b)) + 0.711 * log(pow(a + b, 3)));
+
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
