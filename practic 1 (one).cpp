eb8fe992ed2258325347f08ab3c7fbf3ae86e4ae
sashajsy <115659897+sashajsy@users.noreply.github.com>
2023-09-13 05:16:46 +0300

    Add files via upload

---
+++ practic 1 (one).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int a = 3;
+    double b = 0.151;
+    double pi = 3,14;
+    double result = 0;
+
+    result = (1 - pow(cos(b/a*pi),2))/ (0,5 8 exp(b) + 0.312 * exp (a));
+
+    std::cout << "Result: " << result << std::endl;
+}
